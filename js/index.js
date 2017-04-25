deb79fe4c48cdd333731b0825d2a593532613658
Edgar Mantes <edgar.mantes11@gmail.com>
2017-04-24 22:02:13 -0500

    Added animation to projects and contacts page

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ let increment = 7;
 let decrement = 7;
 let scroller = null;
 let currentPage = 0;
+const jQ = $
 
 // animation variables
 // splash
@@ -56,6 +57,8 @@ let initScroll = (pageId) => {
 
 $(document).ready( () => {
 
+	let count = 0;
+
 	// sticky fixed nav on scroll.
 	let navOffset = $('.nav').offset().top;
 
@@ -71,6 +74,8 @@ $(document).ready( () => {
 	// Scrolling listener
 	$(window).scroll(function(){
 		let scrollPos = $(window).scrollTop();
+		
+		const jQ = $
 		// fixes Nav Bar to top of screen when it reaches the top
 		if (scrollPos >= navOffset){
 			$('.nav').addClass('fixed');
@@ -81,32 +86,106 @@ $(document).ready( () => {
 
 
 
-		if ( (scrollPos >= 0) && (scrollPos < (about * 0.5)) ) {
+		if ( (scrollPos >= -1) && (scrollPos < (about * 0.4)) ) {
 
 			console.log('splash fires')
 			$('.white').css({'animation':'fadeInBack 2s','z-index': '-1', 'opacity': '0'})
-		
-		} else if ((scrollPos > (about * 0.5)) && (scrollPos < (((projects-about) * 0.5)+about))) {
 			
-			console.log('splash disappears about fire')
-			$('.white').css({'animation':'fadeOutBack 2s','z-index': '2', 'opacity': '1'})
-			$('.basketball').css({'animation':'LR 2s','opacity':'1'});
-			$('.traveling').css({'animation':'LR 2s','opacity':'1'});
-			$('.art').css({'animation':'LR 2s','opacity':'1'});
-			$('.lakers').css({'animation':'RL 2s','opacity':'1'});
-			$('.surfing').css({'animation':'LR 2s','opacity':'1'});
-			$('.sky_diving').css({'animation':'LR 2s','opacity':'1'});
-			$('.thailand').css({'animation':'LR 2s','opacity':'1'});
 
+			$('.about-cont').css({'display':'none'});
+			setTimeout( (jQ) => {				
+				$('.traveling, .art, .about-par, .about-quote').css({'opacity':'0'});			
+			}, 2000)
+
+		} else if ((scrollPos > (about * 0.45)) && (scrollPos < (((projects-about) * 0.5)+about))) {			
+			console.log('splash disappears about fire');
+
+			$('.white').css({'animation':'fadeOutBack 2s','z-index': '2', 'opacity': '1'})
+
+			$('.project').css({'display':'none','animation':'fadeOutBack 2s'});
+
+		
+			$('.about-cont').css({'display':'','animation':'unset'});
+			$('.about-cont').css({'display':'block'});
+			setTimeout( () => {
+				$('.traveling, .art, .about-par, .about-quote').css({'opacity':'1'});
+			}, 2000)
+
+			$('.contacts-cont').css({'display':'none'})
 
 		} else if ( (scrollPos > (((projects-about) * 0.5)+about)) && (scrollPos < (((contacts-projects) * 0.5)+projects)) ) {
 			console.log('about disappears projects fire')
-		} else if ( (scrollPos > (((contacts-projects) * 0.5)+projects)) ){
-			console.log('projects disappears contacts fire')
+
+			// show projects
+			// $('.project').css({'display':'','animation':''});			
+			$('.project').css({'display':'inline-block','animation':''});			
+
+
+
+
+			// $('.about-cont').css({'animation':'LRout 2s'});
+			$('.about-cont').css({'display':'none'});
+			setTimeout( () => {
+				$('.traveling, .art, .about-par, .about-quote').css({'opacity':'0'});			
+			}, 2000)
+
+		} else if ( (scrollPos > (((contacts-projects) * 0.8)+projects)) ){
+			console.log('projects disappears contacts fire');
+
+			$('.contacts-cont').css({'display':'block'})
+
+			// hide projects
+			$('.project').css({'display':'none'});
+
 		}
-
-
 	})	
+
+	/*************************************************/
+
+	 /************************************************/ 
+
+
+	// on page refresh 
+	if ( (scrollPos >= 0) && (scrollPos < (about * 0.5)) ) {
+
+		console.log('splash fires')
+
+		// setTimeout( (jQ) => {
+			
+			$('.traveling, .art, .about-par, .about-quote').css({'opacity':'0'});
+			$('.about-cont').css({'display':'none'});
+		// }, 400)
+
+
+
+
+	} else if ((scrollPos > (about * 0.4)) && (scrollPos < (((contacts-projects) * 0.35)+about))) {	
+		console.log('splash disappears about fire')
+		$('.white').css({'animation':'fadeOutBack 2s','z-index': '2', 'opacity': '1'})
+	
+		$('.about-cont').css('display','block');
+		
+		setTimeout( (jQ) => {
+			// $('.about-cont').css('animation','');
+			$('.traveling, .art, .about-par, .about-quote').css({'opacity':'1'});
+		}, 600)
+
+
+	} else if ( (scrollPos > (((projects-about) * 0.5)+about)) && (scrollPos < (((contacts-projects) * 0.5)+projects)) ) {
+		console.log('about disappears projects fire')
+
+		$('.project').css({'display':'inline-block'});	
+
+	} else if ( (scrollPos > (((contacts-projects) * 0.5)+projects)) ){
+		console.log('projects disappears contacts fire')
+
+		$('.contacts-cont').css({'display':'block'})
+	}
+
+
+
+
+
 
 	// nav buttons
 	$('.js-splash').on('click', () => {
